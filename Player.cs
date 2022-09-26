d449efa218cdd9bc543052789accc8d2e39feb13
cantgetin <84032933+cantgetin@users.noreply.github.com>
2022-09-26 23:38:24 +0500

    Добавьте файлы проекта.

---
+++ Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OWC_Calculator
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int NM { get; set; }
+        public int HD { get; set; }
+        public int HR { get; set; }
+        public int DT { get; set; }
+        public int FM { get; set; }
+        public Player(string n, int nm, int hd, int hr, int dt, int fm)
+        {
+            Name = n;
+            NM = nm; HD = hd; HR = hr; DT = dt; FM = fm;
+        }
+    }
+}
